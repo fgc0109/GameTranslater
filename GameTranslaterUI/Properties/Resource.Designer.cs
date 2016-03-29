19361ede0f78d618bdffeae59d5a0255f4811460
camus feng <camus.f@mechanist.co>
2016-03-29 15:30:04 +0800

    更改背景

--- GameTranslaterUI/Properties/Resource.Designer.cs
+++ GameTranslaterUI/Properties/Resource.Designer.cs
@@ -59,15 +59,5 @@ namespace GameTranslaterUI.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   查找 System.Drawing.Bitmap 类型的本地化资源。
-        /// </summary>
-        public static System.Drawing.Bitmap background {
-            get {
-                object obj = ResourceManager.GetObject("background", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
