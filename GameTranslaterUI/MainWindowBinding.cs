c199b490dcd15908e2776f12402f9f3b8b0ee82c
camus feng <camus.f@mechanist.co>
2016-04-01 17:48:52 +0800

    测试

--- GameTranslaterUI/MainWindowBinding.cs
+++ GameTranslaterUI/MainWindowBinding.cs
@@ -19,8 +19,15 @@ namespace GameTranslaterUI
 {
     public partial class MainWindow : Window
     {
+        BitmapImage m_imageDatabase = null;
+        BitmapImage m_imageDataSets = null;
+        List<BitmapImage> m_imageResources = new List<BitmapImage>();
+
         public void BindingState()
         {
+            m_imageResources.Add(new BitmapImage(new Uri("pack://application:,,,/Resources/state_active.png")));
+            m_imageResources.Add(new BitmapImage(new Uri("pack://application:,,,/Resources/state_inactiv.png")));
+
             BasicInfo.m_stateDatabase = true;
             textBox0.SetBinding(TextBox.TextProperty, new Binding(".") { Source = BasicInfo.m_stateDatabase });
             textBox1.SetBinding(TextBox.TextProperty, new Binding(".") { Source = BasicInfo.m_stateDataSets });
@@ -28,13 +35,15 @@ namespace GameTranslaterUI
             //button.SetBinding(Button.IsPressedProperty, new Binding(".") { Source = BasicInfo.m_stateDatabase });
             image1.SetBinding(System.Windows.Controls.Image.VisibilityProperty, new Binding(".") { Source = BasicInfo.m_stateDatabase });
 
-            image1.SetBinding(System.Windows.Controls.Image.VisibilityProperty, new Binding(".")
-            {
-                Source = BasicInfo.m_stateDatabase ? BasicInfo.m_stateDatabase : BasicInfo.m_stateDatabase
-            });
+            image1.SetBinding(ImageSource, new Binding(".") { Source = m_imageDatabase });
 
             BasicInfo.m_stateDatabase = false;
+            m_imageDatabase = m_imageResources[0];
+
+           
             BasicInfo.m_stateDataSets = false;
+
+
 
             image.Source = new BitmapImage(new Uri("pack://application:,,,/Resources/state_active.png"));
         }
