44c1c5cde5b60936f62eb992bac8d7c214d3b4bf
fgc0109 <fgc0109@hotmail.com>
2016-04-20 00:18:34 +0800

    test

--- GameTranslaterUI/RuneCalculate.cs
+++ GameTranslaterUI/RuneCalculate.cs
@@ -15,7 +15,7 @@ namespace GameTranslaterUI
 
         private double[] m_quality = new double[6] { 1, 1.5, 2.25, 3.25, 4.5, 6 };
 
-        private double[,] m_LVL = new double[16, 6]
+        private int[,] m_LVL = new int[16, 6]
         {
             {4, 1, 0, 0, 0, 0},
             {0, 5, 0, 0, 0, 0},
@@ -35,7 +35,7 @@ namespace GameTranslaterUI
             {0, 0, 0, 0, 0, 5},
           };
 
-        List<int[]> lst_Combination = null;
+        List<int[]> lst_Combination_A = null;
         List<int[]> lst_Permutation = null;
 
         public string Calculate(out bool state,int lv, string strMHP, string strATT, string strDEF, string strRES)
@@ -47,33 +47,60 @@ namespace GameTranslaterUI
             string strRes = "";
             RuneCalculate runeInfo = new RuneCalculate();
 
+            string[] a = new string[10];
             //取组合C10,5
             int[] arrayCom = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-            lst_Combination = PermutationAndCombination<int>.GetCombination(arrayCom, 5);
+            lst_Combination_A = PermutationAndCombination<int>.GetCombination(arrayCom, 5);
 
-            for (int i = 0; i < lst_Combination.Count; i++)
+            for (int i = 0; i < lst_Combination_A.Count; i++)
             {
                 MHP = ATT = DEF = RES = 0;
 
                 //取排列A5,X
-                int[] arrayPar = lst_Combination[i];
-                lst_Permutation = PermutationAndCombination<int>.GetPermutation(arrayPar, lv);
+                int[] arrayPar = new int[] { 0, 1, 2, 3, 4 };
 
-                for (int j = 0;  j< lst_Combination.Count; j++)
+                for (int quality = 0; quality < 6; quality++)
+                {
+
+                    lst_Permutation = PermutationAndCombination<int>.GetPermutation(arrayPar, m_LVL[lv, quality]);
+                }
+
+
+               
+
+                for (int j = 0;  j< lst_Combination_A.Count; j++)
                 {
 
                 }
 
+
+                
+
+                foreach (var item in lst_Combination_A[i])
+                {
+                    a[lst_Combination_A[i][item]] = "bai";
+                }
+                foreach (var item in lst_Permutation[i])
+                {
+                    a[lst_Combination_A[i][item]] = "lv";
+                }
+
+
+
                 double[] result = new double[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
                 foreach (int item in result)
                     result[item] = 0.0;
-                foreach (var item in lst_Combination[i])
+
+
+                foreach (var item in lst_Combination_A[i])
                 {
                     MHP = MHP + runeInfo.m_MHP[item];
                     ATT = ATT + runeInfo.m_ATT[item];
                     DEF = DEF + runeInfo.m_DEF[item];
                     RES = RES + runeInfo.m_RES[item];
                 }
+
+
                 if (double.Parse(strMHP) + double.Parse(strATT) + double.Parse(strDEF) + double.Parse(strRES) != 5)
                 {
                     state = false;
@@ -87,7 +114,7 @@ namespace GameTranslaterUI
                     DEF == double.Parse(strDEF) &&
                     RES == double.Parse(strRES))
                 {
-                    foreach (var item in lst_Combination[i])
+                    foreach (var item in lst_Combination_A[i])
                     {
                         result[item] = 1;
                     }
