5eb7e49bf38d713e7b6e4ed9f12ac64dd16e42f4
camus feng <camus.f@mechanist.co>
2016-07-06 20:54:25 +0800

    test

---
+++ GameTranslaterUI/Converter/ConverterImage.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace GameTranslaterUI.Converter
+{
+    class ConverterImage : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string severity = value as string;
+            if (severity != null)
+            {
+                if (severity == "SQLite")
+                    return ResourceHelper.GetBitmapImage("database_sqlite.png");
+                else if (severity == "MySQL")
+                    return ResourceHelper.GetBitmapImage("database_mysql.png");
+                else
+                    return null;
+            }
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        private string databaseType = "";
+        public string DatabaseType
+        {
+            get { return databaseType; }
+            set { databaseType = value; }
+        }
+    }
+}
---
+++ GameTranslaterUI/Converter/ResourceHelper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace GameTranslaterUI.Converter
+{
+    class ResourceHelper
+    {
+        static BitmapImage databaseMySQL = new BitmapImage(new Uri("pack://application:,,,/Resources/state_active.png"));
+
+        public static BitmapImage GetBitmapImage(string name)
+        {
+            return new BitmapImage(new Uri("pack://application:,,,/Resources/" + name));
+        }
+    }
+}
